d7294b938884ec632311b2396ec6f72fa9ebdb70
Dorcohen777 <dorcohen1337@gmail.com>
2024-05-15 21:13:58 +0300

    added animated grid

--- src/views/about.jsx
+++ src/views/about.jsx
@@ -13,14 +13,26 @@ import { useEffect, useRef, useState } from 'react';
 
 export function About() {
     const revealRef = useRef();
+    const gridRef = useRef();
     const [hasAnimated, setHasAnimated] = useState(false);
 
     useEffect(() => {
+        // Grid animation setup
+        const gridElements = gridRef.current.children;
+        anime({
+            targets: gridElements,
+            scale: [
+                { value: .1, easing: 'easeOutSine', duration: 500 },
+                { value: 1, easing: 'easeInOutQuad', duration: 1200 }
+            ],
+            delay: anime.stagger(200, { grid: [14, 5], from: 'center' }),
+            loop: false // Set to false if you don't want it to loop
+        });
+
+        // IntersectionObserver setup
         const observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
-
                 if (entry.isIntersecting && !hasAnimated) {
-
                     splitFunction({ target: '.splt', reveal: true });
                     anime({
                         targets: '.reveal',
@@ -59,8 +71,8 @@ export function About() {
                         <h1 className='splt' ref={revealRef}>Who Am I?</h1>
                         <p >Nice to meet you :) i'm Dor a passionate full-stack web developer, i love what i do and i'm always trying to improve.</p> <br />
                         <p >I love to work, have good communication skills, attention to detail, and good problem-solving and technical skills</p> <br />
-                        <p >Contact Information</p>
-                        <div className='contact-information-container ' >
+                        <p className='contact-title'>Contact Information</p>
+                        <div className='contact-information-container' >
                             <p><FaPhoneSquare /> 053-7171650 </p>
                             <p><MdOutlineAlternateEmail /> dorcohen1337@gmail.com</p>
                         </div>
@@ -71,8 +83,13 @@ export function About() {
 
                 <div className="about-img-container">
                     <img src={me} className='my-image' />
+
+                    <div className='grid-container' ref={gridRef}>
+                    {[...Array(14 * 5)].map((_, i) => <div key={i} className='grid-element' />)}
+                </div>
                 </div>
 
+                
             </div>
         </section>
     )
