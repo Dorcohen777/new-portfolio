7bf7c6cbb05db47e2f4d4680d3495eba728a2b9e
Dorcohen777 <dorcohen1337@gmail.com>
2024-05-21 23:16:02 +0300

    added img srcs

--- src/services/main-service.js
+++ src/services/main-service.js
@@ -10,9 +10,9 @@ const projectsData = [
           'https://www.youtube.com/watch?v=toopP2GD5u4&ab_channel=onio907',
        ],
        imgs: [
-          'https://previews.dropbox.com/p/thumb/ACRxU2zkc0rHw2yF-08UD0C6LkXw9yf58wKWA6XS7SdeZuCaxYm6OmfGqj4td5vQB_RUE10XusZcGnAh48-kbGRVD04aB35hg4A_kP9fttdptph86T_zZzOTZI1GeqqNCu7yPMxhYwp5psvcDQYs8j3Y9PMJrELeFCzErgQcfUqs9QYSYNsk7VHmxDM9px79aJf331nANJES-agrr1XW6qqscuHKljiI4CNUhe1FNDhkkvIEmrBZJGwHxEtukK7moNTJbNSRF7C6DWBkyN8W_udnKtGAmfoQDCotoeyHwT-mB-Binavo849KqdRJi3rl0T5I9z3QRgsjAFsP6QcDn39z/p.png',
-          'https://previews.dropbox.com/p/thumb/ACT_gyoq7ekT_CeSD4bWVeOqbEGI3zsUakCQyUqwTvNc13vdAOYdNP7vrcdMcDaSz-PqIMM9_sk1ZUh3GooIeaJge-W9TJeIo7RJp2Tv8zkS_Zg19OeDTNwQidpGeLSjScogcnzyQ5ZiAaEPtQusIyO-7GdiVMg6eS-KPxOmWFDWZbMyYO_9RPUnSLEGjohgkeXm7y97TKLJR06F58nHcgj9NWjBQ1pzNBr5Q8HxBTFIVKqFs2REEKVqu6kCx4yM3Sm5Xu6ceY6VL542R84hlgXLxS1f7uR-34vNO3aiPxvx0mT4JUBAaYxA5PokYpcnuiKFRNoBkWfD6rDTNW2WkrNE/p.png',
-          'https://previews.dropbox.com/p/thumb/ACTy3cNgNhNbOX0m6Zg1SkjsKwK0hwZKiXpanTdHiNmHJRzj0MPuf6gaFMQ7lzu2or12MZStzD1Ic2iGuSWJF7toDhtmYqhJcNbeJgWxwKhT6KjU5YijKLZ59dXm1rXbajrnQSoPiUqx26uQzeZOsPcQ_0A0a-CVak9j6ftXWjhQN33Uj49wnF7m-5USeT0mI5UUxPGKMqbQyJ_gnjrrP_EvgOH4QRKxYcpd7AmppN_4AdSXVobDxH_8Hv4S24ms2Rc2dFG3j6pXFlPpZ8te8zCZc1IvqecH3oUEPXk0ZG97rkSu2vnSmEBXcKhwQWACaqDZI239e9YC0G4g9J26C-nn/p.png',
+          'https://iili.io/JildydB.png',
+          'https://iili.io/JildmgV.png',
+          'https://iili.io/Jil295P.png',
        ],
        tech: ['react', 'html', 'javascript', , 'mongodb', 'nodejs', 'sass'],
     },
@@ -24,9 +24,9 @@ const projectsData = [
          'https://go.dailyinvesting.net/pt-br/criptoblindersun/'
       ],
       imgs: [
-         'https://previews.dropbox.com/p/thumb/ACQHc5FZPjevotpFoj10zUIQHwea2bWjt5xnaEAqsyi49ihO9B1MdCNPfHijxyD5xM4tq0fep5AvcjSAS6DvH3BJ42h2PBqkAtFh-dGpBDb_rhJqmzFGNNcn3F6v66Vfx4WuRCxHSFPnkFMp0ws9dArbBEpCvTMzqPd-TUT14Qt1Wmob-cu9H0y-NS3xlYCgpNS0F_AjGG70VRbGmCYRgahx9FufidbS3ROKeQuN5QVUlPo3eOO91jj5OWMC9WLUwuMelsvcHg9gxxl6c_Q2u9Bv12bySx_5WgElNggpwhFQjUiksEsMRcC26jrSUQhz3DBEl-v3nIPQ_Hmrh124nCnl/p.png',
-         'https://previews.dropbox.com/p/thumb/ACRC3tLSVmBHPZxV9DRSm74VKwBrlNEbANj3BlfbiQBsEuxXq33NmIDsQlzpUuEFkHGXj3pURFsLHAcsrDbR9i6ZPQQBr1Ro-1pSQSfum-V-c7funMbaYooJzMrieWC70G3eQ7LXedKdSUEq-C0IyF81zey8IsgVH57YWfflWALRy48_jadP0V9oofEhjQfFi7OIqQoF70BeOoGbaJ7lx2vbrj_XZ4DHPpNQgHK5EpANf-GsaLT40zagzAqPFSI_uzRXS3Dn_x-jt2mYWImdNRscBtV8bwNOfuvGmDfKVjkQrGPu5JYS5BUVXOJC42aHvXAs3SCGdN-_JgfJSA2FzIfB/p.png',
-         'https://previews.dropbox.com/p/thumb/ACSFg1BazYu66atEnItZt5tcQIxO5Kon112CGB8Z5He_mN-2w2kA5a68Kk3mjxoBmt7vbiXGVu6INDqEwsLUKSUBEi_EDkxWFaS_34TswPPMrIlxcoj9BoSBBEIHSl57q2PjuAHJG-xJUC4BxyFDTtwnHM0TuGNu_qhchY4NLHnRQGDMd5f6RXmzVF8weeyAv8Og9gtXP0Jfsw9v3Srq2kjjXk7Hqd8UvZtChmBwfwtw0vvbj27VLl7KvifNmHkTxBK5ks2ANF5-lUiMPOqfB4G794VD0CuqnyvC3Ste6mqGn94f0tmB-6USua2wWXmgl482t21dhF_uKhJj_7AylQNJ/p.png',
+         'https://iili.io/JilKpCg.png',
+         'https://iili.io/Jilf96J.png',
+         'https://iili.io/JilKDj1.png',
       ],
       tech: ['html', 'css', 'wordpress'],
     },
@@ -38,8 +38,8 @@ const projectsData = [
          'https://lps.dailyvesting.net/es/amazon_reworked_es/'
       ],
       imgs: [
-         'https://previews.dropbox.com/p/thumb/ACR9Q1CZoB8hY2LixqjWu88kUhyREgCTBpTPsvX1W3pjCe64fNPOilFRnjk1jcPFEM0IPE93flMfSMz0750r-Bbg3L07ULbKkI1suSYcRtua-bqHwvyqV8T1IgPTEHYi0vi48hAoklO6Xqf5QAUe3wl2h-hiBOWaWoSPXwfwiTmLuupR8i369UVGkqy0C7phP9yXnbcQKstOziafmBzimeko4RfHnyRB5Ut5XXA-1X6WWd8Iz-1G5-606ggO65fnEgiFuhygH98J8qOR86WJCOhh24qipO2TxXMCoOSzlbwguvP3SAmbKgvWpGVWua9QF7-da_k4WVl38-MI65XvYEjv/p.png',
-         'https://previews.dropbox.com/p/thumb/ACS6RbwPFHVBZGDP60mPnp0GyXmSWm9kAgAKEiPISrUHHD4lr5tW-T2gcaNJ_rcW0MaL5lF4cOAqGnck8gyhUMU6oUAkviN412mgAdfA-IrBUXXtTj4QRxeT7SdELmIFRsIDBWBA0l7VQOAeS0UjXCZZerr1kYxbGt1lRfr3qgURc7tiJu7xYS7dUK_XxH48PZ_6-NmwBvkb2VKyCnqhgKE_MCqcMiUkDZ_fy-XDe_pQxNmVES9K3_MAAW4PdDwCnFX3bM0-3hjk2ODlgYjyTpMZx4ACtGJwd8KmsUAHdLUClJ2Dt1bVVc3MRgwkB_ib3E_df7y3ZwNi1cL7K9_Imp4H/p.png',
+         'https://iili.io/JilC1Fs.png',
+         'https://iili.io/JilClPn.png',
       ],
       tech: ['html', 'css', 'wordpress'],
     },
@@ -54,9 +54,9 @@ const projectsData = [
           'https://www.youtube.com/watch?v=IV3_cN8GbfA&ab_channel=onio907',
        ],
        imgs: [
-          'https://previews.dropbox.com/p/thumb/ACQ2rMprZ7S95iuFuItDPUALgrU3RL3qplesIjyEXJI86ThHNt_SBcmrNC3jkzG2GabEewFaBudSbCyi2ZTTuf5G68Gevu_51ielsH0c_dTjN9Xmz-pGT4jKb4WCMauIRNCJyLed_gHyfoMzDmr8AZwPLTslweBVJz78yjF4YH_o30OJos7OzCM23l5R3K-k1NpTNmk-UqIoznIUuXu_g35EfgUO3KClM7H2uVZIERzdXxyEgfzmhPIrx4xJ7J4G37NV33SSaJU2fUYBpPvZKQQF4Zmbq1dfw4WGQns_hblK7FCpsmR_d_BPjk9jH08ivW3FjrkNp3M5IHcJL1C2pocW/p.png',
-          'https://previews.dropbox.com/p/thumb/ACT2w_N4ULk8P3EX1LXr9lqqHN-phr9dVj--CLrHunHnW2MNP1vFbUM_LhcQU4iJD8FNTEiWIEbDTbwjuADdI3vySVg2twwph__R_9aXPRsiyipZ9wB8B4rE4hrbnoBiMgRCLJIjy32nvM8PyXRxvTjH8zWFtrOGqCmTwm-kRrzNkdIjWZfwoPDLLwXc4LSpJpgRnidmHTtpr0VBPaAwVAHRvXQtye_cL60FDf-NLoeFbfHY8PrPwBKdVb4_pC7-dTQUwehqasgtnlySpvhtXDBKUf7SG6-DbKmtUEhFT9GVJsfZGWBNH1a4GPxfqlh9ZQ0Unak_97orRSGlxKEBBpa4/p.jpeg',
-          'https://previews.dropbox.com/p/thumb/ACQz4E26YFWyaA9bijnsU8ByopiRcPP0YwsDtxbnHYXUC3sfjX4h4clY9gNNWSji8E3YAFFf3GSkqMtMw-Ss1nSzjCACpNKg_kEF5UDu2Abvtdm5KmRpyDpNzWfv_O3LQrhx8rxUbk3Mnhe88ecvmjo83K87YfvWXurIU1k_KSk5MUdJsidsIrED8K0xadClM7py-u2yUDXRC9GCc7GtrWzMHW3vyYGwKACOHAekMHopFHWGXB89GrWgAa9H36wguQ3reLwwraev2SCzLqX2XvgDp1q7TKoPLiEVPAWLhaNlil0DdsAWeJtpO4NGLJErPJUF-2EJJH8wxAEcIzOuZanc/p.png',
+          'https://iili.io/JilCOPe.png',
+          'https://iili.io/JilCNV9.jpg',
+          'https://iili.io/JilCwo7.png',
        ],
        tech: ['react', 'html', 'javascript', 'sass', 'mongodb', 'nodejs'],
     },
@@ -68,8 +68,8 @@ const projectsData = [
        tech: ['html', 'css', 'wordpress'],
        links: ['https://medton-hedim.ru/'],
        imgs: [
-          'https://previews.dropbox.com/p/thumb/ACS8sUb2VsrLu_ClWGz9gu6BmAajZXZ7391v5m_Iue--s8PDRbkXT7MfleyZUyfiCutQPCpQ-CvG_A1F_bE05-xF-1Up1wTE4tG6rbWNQ9gY04h7vNVuIf8TbAIkYuORGKGG_UeZTmBB5Jb-0UExeJHGgv96oN4obPqZbsGEKfCDO_Y5SlIjvSBiYCOgx-yBEPAjHDI1OxTxb54R_MRYsI4nM7OYJzzTgmSk2sT5JYp-We_X8p6kjoBmb_H0P0it_DrCU_H_9UVDiNtrbjowRAifNPkkkyc2W9ImvvGwzj86Lzead0PLAxNyVfJpt-lezUFINC3jfL3gTk9YiOxqEVwi/p.png',
-          'https://previews.dropbox.com/p/thumb/ACTe49oyU-pcN3iZcrogxP3Or_YjaYuIpVeXik8p7m5ItVS7W4LT025vR_kfQ0KBDVpPjqa38BVarBTDi-bukgmNdeJOVejWd2n3Ni-JdYBcw-Q-mmu4yy8P5go-NdP9A_kgIX7j10xN2l7vAKI43eHprP2amsyQVJ7kofYdb_mrh3T2dm1kUHv2y6drEnzj2cfExZwfcpMb-r4SYN3THGh02nW6m34Q5kIAU3qJiL1dVz6kidBtxGtOXtKQo2kAlSaphfoClX3eK0362TcG1611V75knXU0R860eCMPkh7PUh7OoNggd-kSrB531E4zVQmHu8h11IxQnxDgvtYPMh6z/p.png',
+          'https://iili.io/JilCZla.png',
+          'https://iili.io/JilCQKg.png',
        ],
     },
     {
@@ -80,9 +80,9 @@ const projectsData = [
        tech: ['html', 'css', 'wordpress'],
        links: ['https://kids.myhearing.co.il/'],
        imgs: [
-          'https://previews.dropbox.com/p/thumb/ACScMjmbM5EDJGtavQUeTkgkf08-MzPo967tD-wg9IdvcGcHCs5UGrmR7KVknPz6u3d80uS5qHfhm2dCrIB5CYFVWh9TELImEdgBOmEDoqqnMxH9UCK6-UsoH4KHRJXbXxBmxOv5jQj0MbEz4vrB5y-hEcTugocoB5A8nZPldT7YbY5FfDlE5DEimmIT1HcaXbI4STujLNlR5Q_PKpAHShZyy6ekW9gXUdVliPQIerMAvd1dgKKmhgM0Boa9Ovvn7l0fjce-rnK3CKyZmNDphPnSR22tmor1iUimL2XOrlQsAPCOChVzIQYe07d_iMoVFqycQZ0lfRS2doMHuouIcsSl/p.png',
-          'https://previews.dropbox.com/p/thumb/ACSYaIuL96p2Ee8-4zvnfk5xwAjI9LEygDQ-y_XqkzZib-EdtGGmTerT403O9c4Zw4wrRiA8j6bfx9kWiEpcq63qGv5zaoBizZop1Gb1x3CJ1YWnwrY65XZzrksgX_3g75JPA43CYCDKXTG2xY_NMOGpaBNSgeprXmcRlhS04O1YNiH4UhA2xKaA46WWfkskx2BJslmJDR2x0OTOl6Zs-UQCmaf-x5ZckI-28_kF0c6OJSGDICSK-kj1o1v5QQD6q3_GhJFG9FuAamshOe1Rk0WbZVyooAsSF_MsU8wbNB9k5AkpRlQVgXUNJ80aXZpJ3yXJ5E017VG5t3q6uUqMfD7f/p.png',
-          'https://previews.dropbox.com/p/thumb/ACR6N6-QY8ibHKwwcpQi35A7qIcgAFr6-RN8Kk6XaRoR2XZ0U1YQg5g40Z-SF_lox4zFAR0HOKQr8Muz-N2Y7UxQvRxkrs6gLNp9tppFLEx7HRlaNeX8lHcj3shnHojUBAZz2tE6PNzerLc9rx-uhw16byZecj5ByPwlfbPwJROhSUdKD6FPcFAyoaXTGRMt_nTFCHxRPi0QW8m8BS603oUUxcBVn4h_rENtbx_bBac2rCc186kTjDbMq91SK9n7nJmASaBWYYcJ9XzL2J9PNhhfRVqje-SFATueX9uDX6YGs_qNTu4oY3wPQDHS5jzOSvmgWPa7AksFz0ptT02vuQdV/p.png',
+          'https://iili.io/JilnHxI.png',
+          'https://iili.io/JilnJVt.png',
+          'https://iili.io/JilndiX.png',
        ],
     },
     {
@@ -93,8 +93,8 @@ const projectsData = [
          'https://go.dailyinvesting.net/pt-br/amazon/'
       ],
       imgs: [
-         'https://previews.dropbox.com/p/thumb/ACQt9k4eEz1N6o3XNBm7M7qvuGt6zqoEszwSjXfE5OAerNFWqWdXKku6wppvPpjaGQkFOynheur6dql_CHHswhUbz-DYLyUlwSzg14MtawXseRpwsp5bgElkOVMXUKPTSV1y6Kt4WXNfIueEt-KNtTuusPnRlesRBwxQA0d-lVXM58djMDC3xf1_VvqcwjJofnP_wKZ5CnEU_41994S9YZumL9fRs2U8uYe0A6kDNfDbawHMVDG637_S6DCS-W6ctBYZlhWD88J2yU8lsKR_hbBMPJBR6AwUjzpOoml-DJc2s06yEzeZ00u_zX6tn2xF7ueEe0VpB9V5lxlxnEdoJdvw/p.png',
-         'https://previews.dropbox.com/p/thumb/ACS1ZKf10r1ImTic2nX74PTe54UT0L4ggxBI_i4OzRILhsReRCUle_y7ISrj9LM3XgFtVSjv8ZS5SnDmqScBpRyJiT2SxBRge1MXBkgcD1RS0abbB9T47-LSWxc44y_5sybr_aNc48DlNrgKzabvrGEPp2RKz4nob56MxQ5TnzuEsSRo7YOTc8wQ7AftfGT94qkPW4xeBwC9Jyd5wv0lxrMnXgBvlI30wrA8NMs3ISke6bTYIF_YEbXWQTPET26u_w72_gakQ0uTTEXOpzA1Zrjucq9kKSzu4TX3ZWBXnUoH0rWgHNB_EcI8g6OBya2FxQmV1BxfTtUEptuxv7X8Q_Wx/p.png',
+         'https://iili.io/JilnA0u.png',
+         'https://iili.io/Jilnufe.png',
       ],
       tech: ['html', 'css', 'wordpress', 'javascript'],
     },
@@ -106,7 +106,7 @@ const projectsData = [
          'https://go.dailyinvesting.net/es/cemex/'
       ],
       imgs: [
-         'https://previews.dropbox.com/p/thumb/ACTPwA_somHOunSTe1A6SQxQIM558S8ppxKWyP43lX6J6N4XTJaGegZunppo-aNUOMuZcVr7IqgSuSvyj-7C_MM1XZBYMRg8nErc3hNfmM7AeDFpgdY2U33GVXsNZOBL56t54iSsmyIV5RZIIhcWQOBNLtKzm9IW8h2Y0TMLt1TcNVbga-LXxKp0TvgWXK0Gzp0O2LXd6pGyyCu06KsgJSAIkqDDIdFpch-t8tjPTCEnkphWwFWJmoFKWAYNZCTKuJkQXbTFqfy-pZGF4ZzUHOdrd8tNzDeBQjG0S5ntRrdBqevfA38J7-U03BbTYXYWK4V70BgGaLYj3nceT1SXm8mP/p.png',
+         'https://iili.io/JilnEs1.png',
       ],
       tech: ['html', 'css', 'wordpress', 'javascript'],
     },
@@ -120,9 +120,9 @@ const projectsData = [
           'https://github.com/Dorcohen777/AppSus',
        ],
        imgs: [
-          'https://previews.dropbox.com/p/thumb/ACS7wSJsvvPGitrzbxLILbyzUQ8rYy9yGxxRx13YKcBLZiSYq0hK0nYLo4Js6hJIb6gAINHs0clTEVoqF9HjQp6lwCEPd70qTnB9IW1EXeLCtrZ5QqfpyKb1dLD695KHQJ6nv73ajcRB6DvPp2qgr-mCvFoV4veZCz0hUyJJ_DH8-bBbaVCbS2mTMiL65cTBfldhkRnc9pET5uklOqXH-tTqLisuVHHrxQogIrbNloPVZralHlrDKMDn3Ydn3Doj2ANWuHK4zYT4A9exOGDjeXXEIcyyYbvRFZmyFLqYkV_4kCaCkp2BMs21MwKmzlErIvjAluzOJ9LMzlger0ITS3pR/p.png',
-          'https://previews.dropbox.com/p/thumb/ACQ9p1wnJR-f6sheFT3Nx8iWtERZ89ZoMpHlK5Swqy08hUwLysZjvso2BSeaYWYr2T_fRafVlpQj5G0aJTvZv66jxrrC5o7QI1SdQwp9v5ahI8L0u9_pYXnYNN3qLkhyIV2_mUnpTYzWh5jSiERZ68wTkHhBCufnEX4QuTQQtdVLm_jfGAPpoTf2y84nKtqz0lztj157hwSg37zU1JSPSU5EnW27K86fmIdr3kJMAzI4YaFE6gAv5Gj6ENilvE4_bYoNAZE3SqP2dMJMl1nc5K_X6t21h0P5ZsZK-wbOd906psGMY5wLoIIoBZzArgbqLfczW226KFgiqnrbU1tBLMny/p.png',
-          'https://previews.dropbox.com/p/thumb/ACQdQVwvyT4gg6p4DDAdTF4RtxxYeSvVzFQfTxUp2CIfnL0OmhjfbdLcCBAEsojQFKYr2B7S1T1x9jfxrifNrY7jI6ZCppo2RpY9lwzsP7iGI_W2YTh5fYXlJ-5Dmwm9Sjd7FeDujbdOMbL_z7Wa_vaDpiTHHfkyJmhXeqcmdObrJYrVW6F83RyorX2Ble2VHbLkE3XVd21RXV5XitnAlFF3F5-XQY6PhDcaWv4O3_fzU0J8PgU4IGbKq7t0RqqIoTDHXksqsVikqsypldSvgv4xUWkxF4Uf0vjQe_wI9wgnqjWb-QGTTdKvL7ZtGuASSYmOtflWDrPhx8S6MuX77X48/p.jpeg',
+          'https://iili.io/Jilnvst.png',
+          'https://iili.io/JilnkXI.png',
+          'https://iili.io/JilnSqX.jpg',
        ],
        tech: ['react', 'html', 'javascript', 'css'],
     },
@@ -137,7 +137,7 @@ const projectsData = [
           'https://github.com/Dorcohen777/mineSweeper',
        ],
        imgs: [
-          'https://previews.dropbox.com/p/thumb/ACRbrvO5UoLw94iPLN0fgAoZ59HHSnizyAGevKZ5xS3WeKMSG4nb1OwFqDRJGjck4Wcrkl6TCRHpkCoELMIFkFYekdnu6V-Lif5qTe1x0Hl6M1E6zrdDnSQ8XkGYc8zxMRwxs4UuHsu8csKxfpYJN0UUTJML3v5slHazH1wL3nga0hSvmfTBNqH0Q0qAbz7uoLrSaUpj3CywTorbmuIkXfVUDCzLFk7EGz6JYkEewBYdSp90oyNtern_1gAXyno4e5Vrcs8UpNkzkv3KyjMHKmzg8ZQed7HpYzmPfD1Mp-ZMJLVAXPZVgIqiQic2Bt_0-bPZHre9skI2JvlVpwIXfCe1/p.png',
+          'https://iili.io/Jilniml.png',
        ],
     }
  ]
